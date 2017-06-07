7bb5d5840ece124c79fa9097cace8f6ece3b9b4a
吳剛志 <chicken@chicken-house.net>
2017-06-07 22:35:02 +0800

    draft

--- restart.cmd
+++ restart.cmd
@@ -4,8 +4,7 @@ docker restart columns
 
 :wait
 docker logs columns -t
+@echo --
 @choice /T 5 /D N /C YN /M "Press Y for re-generate web site."
-: @echo %ERRORLEVEL%
 if %ERRORLEVEL% EQU 1 goto start
-: @pause
 goto wait
