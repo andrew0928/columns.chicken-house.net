a75b52cd379a7475ee8a685053af37c9aeef1e32
吳剛志 <chicken@chicken-house.net>
2017-06-17 22:44:52 +0800

    update restart script, add 'cancel' option

--- restart.cmd
+++ restart.cmd
@@ -5,6 +5,9 @@ docker restart columns
 :wait
 docker logs columns -t
 @echo --
-@choice /T 5 /D N /C YN /M "Press Y for re-generate web site."
+@choice /T 5 /D N /C YNC /M "Press Y for re-generate web site. (N: do not re-gen, C: cancel looping)"
 if %ERRORLEVEL% EQU 1 goto start
+if %ERRORLEVEL% EQU 3 goto end
 goto wait
+
+:end
