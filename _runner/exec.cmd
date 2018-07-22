0bd7807033947c5a802326b62a20b18d47da0e6c
Andrew Wu <chicken@chicken-house.net>
2018-07-23 03:58:21 +0800

    update: migrate to LCOW

--- _runner/exec.cmd
+++ _runner/exec.cmd
@@ -1,12 +1,11 @@
 SET CONTAINER_NAME=demo
+SET IMAGE_TAG=jekyll-3.8.1
 
-docker build -t jekyll:test .
-docker run --name %CONTAINER_NAME% -d -p 4000:4000 jekyll:test
-
-
+docker build -t andrew0928/columns:%IMAGE_TAG% .
+docker run --name %CONTAINER_NAME% -d -p 4000:4000 andrew0928/columns:%IMAGE_TAG%
 
 docker exec -w /home         %CONTAINER_NAME% git clone https://github.com/andrew0928/columns.chicken-house.net columns
 docker exec -w /home         %CONTAINER_NAME% chmod 777 columns
-docker exec -w /home/columns %CONTAINER_NAME% git co draft
+docker exec -w /home/columns %CONTAINER_NAME% git checkout draft
 docker exec -w /home/columns %CONTAINER_NAME% git pull --all
-docker exec -w /home/columns %CONTAINER_NAME% -i -t b9 jekyll s --watch --drafts --incremental --destination /tmp --unpublished
+docker exec -w /home/columns -i -t  %CONTAINER_NAME% jekyll s --watch --drafts --incremental --destination /tmp --unpublished
