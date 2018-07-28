0b0db9fd7ba29232886a7e156419452e04c493c3
Andrew Wu <chicken@chicken-house.net>
2018-07-29 00:46:55 +0800

    update script

--- run.cmd
+++ run.cmd
@@ -7,4 +7,6 @@
 : docker run -d --name columns -v %CD%:/srv/jekyll -p 4000:4000 jekyll/jekyll:2.4.0
 
 
-docker run --name columns -d --platform=linux -v %CD%:/srv/jekyll -p 4000:4000 jekyll/jekyll jekyll s --watch --drafts --incremental  --destination /tmp --unpublished
+::::: docker run --name columns -d --platform=linux -v %CD%:/srv/jekyll -p 4000:4000 jekyll/jekyll jekyll s --watch --drafts --incremental  --destination /tmp --unpublished
+
+docker run --name columns -d -p 4000:4000 --entrypoint jekyll andrew0928/columns:20180723-jekyll-3.8.1 s --watch --drafts --incremental --source /home/source --destination /tmp --unpublished
